970538a74de81d01d05b924aba4ace9fddaf47a8
Денис <1989nekutsiy@gmail.com>
2022-11-23 14:18:30 +0300

    new HW for C#

---
+++ Ex_01/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int i = number.Length;
+
+if (i == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("Да, " + number + " -> палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} -> НЕ палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Число " + number +  " не является пятизначным");
+}
